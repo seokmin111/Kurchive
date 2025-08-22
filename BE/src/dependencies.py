54479c81a1cdea7ae9715845106c881e84954fa7
jwhong1020 <jwhong1020@gmail.com>
2025-08-22 16:33:14 +0900

    is_admin 수정

--- BE/src/dependencies.py
+++ BE/src/dependencies.py
@@ -105,9 +105,10 @@ async def get_current_user_from_token(
 async def get_current_admin_user(
     current_user: User = Depends(get_current_user_from_token)
 ) -> User:
-    if not (current_user.token_scope == "admin" and current_user.is_admin is True):
+    if not (current_user.token_scope == "admin" and current_user.role == "admin"):
         raise HTTPException(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Admin access required",
         )
     return current_user
+
