b36482aaa2b3b0a6cd2259a76c7b0d151bdaa8b8
YH <dbsgml0321@korea.ac.kr>
2025-08-26 00:53:22 +0900

    비동기화 마저 함

--- BE/src/main.py
+++ BE/src/main.py
@@ -2,38 +2,40 @@ from dotenv import load_dotenv
 load_dotenv()  # .env 파일의 환경변수를 로드
 
 from fastapi import FastAPI
-
 import os
-
 import logging
 from fastapi.staticfiles import StaticFiles
 
-import BE.src.models
-from BE.src.routers import user, map, recipe, mypage, restaurant, admin, admin_auth
-from BE.src.database import Base, engine
+from BE.src.database import Base, async_engine, set_sqlite_pragmas
 
-import BE.src.models.tags   # 모델 먼저 import
-import BE.src.models.restaurants
 import BE.src.models.users
+import BE.src.models.tags 
 import BE.src.models.regions
+import BE.src.models.restaurants
 import BE.src.models.admin_config
 
-
+from BE.src.routers import user, map, recipe, mypage, restaurant, admin, admin_auth
 from BE.src.errors import register_exception_handlers
 
 
 
 # 모델 import 끝난 후에 create_all 실행
-Base.metadata.create_all(bind=engine)
+# Base.metadata.create_all(bind=engine)
 
 logging.basicConfig(
     level=logging.DEBUG,
     format="%(levelname)s:%(name)s:%(message)s"
 )
 
-
-
 app = FastAPI()
+
+@app.on_event("startup")
+async def on_startup():
+    await set_sqlite_pragmas()
+
+    async with async_engine.begin() as conn:
+
+        await conn.run_sync(Base.metadata.create_all)
 
 os.makedirs("uploads", exist_ok=True)       # 선택(폴더 자동 생성)
 app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")  # 
