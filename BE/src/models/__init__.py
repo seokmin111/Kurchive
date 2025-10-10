3932bed990cedf64997852854aed2a2ce8ffce58
jwhong1020 <jwhong1020@gmail.com>
2025-10-10 13:44:55 +0900

    모든 모델 import

--- BE/src/models/__init__.py
+++ BE/src/models/__init__.py
@@ -4,6 +4,10 @@ from .role_permissions import RolePermission
 from .favorites import Favorite
 from .restaurants import Restaurant
 from .recipes import (
-    Recipe, RecipeStep, RecipeStepImage, RecipeIngredient, 
+    Recipe, RecipeStep, RecipeStepImage, RecipeIngredient,
     Ingredient, Unit, UnitConversion, IngredientUnit
 )
+from .tags import Tag
+from .regions import Region
+from .comments import Comment
+from .admin_config import AdminConfig
