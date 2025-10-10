8a0168df3c896eee2b000799b66e768f21ce0296
choieunu <cnstela@naver.com>
2025-10-10 22:07:14 +0900

    Add files via upload

---
+++ FE/restaurant_archive/page.tsx
@@ -0,0 +1,174 @@
+import styles from './page.module.css';
+import Link from 'next/link';
+import axios from 'axios';
+
+export const metadata = {
+    title: '커카이브 : 레시피 아카이브',
+    description: '커카이브 레시피 아카이브',
+};
+
+export default function MainPage() {
+    return (
+        <main className={styles.nomrg}>
+            <div className={styles.header}>
+                <br />
+                <h1 className={styles.title} style={{ display: 'inline' }}>커카이브</h1>
+                <p className={styles.sub_title} style={{ display: 'inline' }}>우리만의 미식 지도</p>
+            </div>
+
+            <div>
+                <form className={styles.search_container} action="#" method="get"> {/* 검색어 보낼 주소 */}
+                    <input type="text" className={styles.input_box} placeholder="식당 정보를 입력해주세요" />
+                    <button type="submit" className={styles.search_btn}></button>
+                </form>
+            </div>
+
+            <div className={styles.button_wrapper}>
+                <Link href = "../">
+                    <button className={styles.back_btn}>&lt;<br />메인화면으로 <br /> 돌아가기</button>
+                </Link>
+                
+                <Link href = "/restaurant_archive/archiving">
+                    <button className={styles.red_btn}>식당 아카이빙</button>
+                </Link>
+            </div>
+
+            <div className={styles.recipe_container}>
+                <div className={styles.recipe_item}>
+                    <div className={styles.recipe_contentCarrier}>
+                    <h4 className={styles.recipe_title}>음식 이름</h4>
+                    <div className={styles.recipe_text}>뭔가 내용이 들어갈듯</div>
+                    <div className={styles.recipe_descriptionContainer}>
+                        <div className={styles.recipe_icon}></div>
+                        <div className={styles.recipe_underContainer}>
+                            <div className={styles.recipe_reviewer}>reviewer name</div>
+                            <div className={styles.recipe_date}>Date</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className={styles.recipe_item}>
+                    <div className={styles.recipe_contentCarrier}>
+                    <h4 className={styles.recipe_title}>음식 이름</h4>
+                    <div className={styles.recipe_text}>뭔가 내용이 들어갈듯</div>
+                    <div className={styles.recipe_descriptionContainer}>
+                        <div className={styles.recipe_icon}></div>
+                        <div className={styles.recipe_underContainer}>
+                            <div className={styles.recipe_reviewer}>reviewer name</div>
+                            <div className={styles.recipe_date}>Date</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className={styles.recipe_item}>
+                    <div className={styles.recipe_contentCarrier}>
+                    <h4 className={styles.recipe_title}>음식 이름</h4>
+                    <div className={styles.recipe_text}>뭔가 내용이 들어갈듯</div>
+                    <div className={styles.recipe_descriptionContainer}>
+                        <div className={styles.recipe_icon}></div>
+                        <div className={styles.recipe_underContainer}>
+                            <div className={styles.recipe_reviewer}>reviewer name</div>
+                            <div className={styles.recipe_date}>Date</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className={styles.recipe_item}>
+                    <div className={styles.recipe_contentCarrier}>
+                    <h4 className={styles.recipe_title}>음식 이름</h4>
+                    <div className={styles.recipe_text}>뭔가 내용이 들어갈듯</div>
+                    <div className={styles.recipe_descriptionContainer}>
+                        <div className={styles.recipe_icon}></div>
+                        <div className={styles.recipe_underContainer}>
+                            <div className={styles.recipe_reviewer}>reviewer name</div>
+                            <div className={styles.recipe_date}>Date</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className={styles.recipe_item}>
+                    <div className={styles.recipe_contentCarrier}>
+                    <h4 className={styles.recipe_title}>음식 이름</h4>
+                    <div className={styles.recipe_text}>뭔가 내용이 들어갈듯</div>
+                    <div className={styles.recipe_descriptionContainer}>
+                        <div className={styles.recipe_icon}></div>
+                        <div className={styles.recipe_underContainer}>
+                            <div className={styles.recipe_reviewer}>reviewer name</div>
+                            <div className={styles.recipe_date}>Date</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className={styles.recipe_item}>
+                    <div className={styles.recipe_contentCarrier}>
+                    <h4 className={styles.recipe_title}>음식 이름</h4>
+                    <div className={styles.recipe_text}>뭔가 내용이 들어갈듯</div>
+                    <div className={styles.recipe_descriptionContainer}>
+                        <div className={styles.recipe_icon}></div>
+                        <div className={styles.recipe_underContainer}>
+                            <div className={styles.recipe_reviewer}>reviewer name</div>
+                            <div className={styles.recipe_date}>Date</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className={styles.recipe_item}>
+                    <div className={styles.recipe_contentCarrier}>
+                    <h4 className={styles.recipe_title}>음식 이름</h4>
+                    <div className={styles.recipe_text}>뭔가 내용이 들어갈듯</div>
+                    <div className={styles.recipe_descriptionContainer}>
+                        <div className={styles.recipe_icon}></div>
+                        <div className={styles.recipe_underContainer}>
+                            <div className={styles.recipe_reviewer}>reviewer name</div>
+                            <div className={styles.recipe_date}>Date</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className={styles.recipe_item}>
+                    <div className={styles.recipe_contentCarrier}>
+                    <h4 className={styles.recipe_title}>음식 이름</h4>
+                    <div className={styles.recipe_text}>뭔가 내용이 들어갈듯</div>
+                    <div className={styles.recipe_descriptionContainer}>
+                        <div className={styles.recipe_icon}></div>
+                        <div className={styles.recipe_underContainer}>
+                            <div className={styles.recipe_reviewer}>reviewer name</div>
+                            <div className={styles.recipe_date}>Date</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className={styles.recipe_item}>
+                    <div className={styles.recipe_contentCarrier}>
+                    <h4 className={styles.recipe_title}>음식 이름</h4>
+                    <div className={styles.recipe_text}>뭔가 내용이 들어갈듯</div>
+                    <div className={styles.recipe_descriptionContainer}>
+                        <div className={styles.recipe_icon}></div>
+                        <div className={styles.recipe_underContainer}>
+                            <div className={styles.recipe_reviewer}>reviewer name</div>
+                            <div className={styles.recipe_date}>Date</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                <div className={styles.recipe_item}>
+                    <div className={styles.recipe_contentCarrier}>
+                    <h4 className={styles.recipe_title}>음식 이름</h4>
+                    <div className={styles.recipe_text}>뭔가 내용이 들어갈듯</div>
+                    <div className={styles.recipe_descriptionContainer}>
+                        <div className={styles.recipe_icon}></div>
+                        <div className={styles.recipe_underContainer}>
+                            <div className={styles.recipe_reviewer}>reviewer name</div>
+                            <div className={styles.recipe_date}>Date</div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                
+            </div>
+
+            <div className={styles.footer}></div>
+        </main>
+    );
+}
+//const data:[] = 
