c88024f9ff0c2c348c10b46764e57b404aed267c
jwhong1020 <jwhong1020@gmail.com>
2025-10-31 18:20:19 +0900

    도커 수정내역 잔뜩

--- BE/DB/migrate_sqlite_to_mysql.py
+++ BE/DB/migrate_sqlite_to_mysql.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 
 
 
-load_dotenv(os.path.join(BASE_DIR, ".env"))  # 루트의 .env 로드
+load_dotenv(os.path.join(BASE_DIR, ".dev.env"))  # 루트의 .env 로드
 
 # SQLite 경로 (필요시 수정)
 SQLITE_PATH = os.path.join(BASE_DIR, "BE", "DB", "Data.db")
