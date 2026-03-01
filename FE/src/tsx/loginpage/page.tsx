3ea23a734ecb2d24193cb503575ed64daa277423
jwhong1020 <jwhong1020@gmail.com>
2026-03-01 19:37:44 +0900

    배너 문구 추가

--- FE/src/tsx/loginpage/page.tsx
+++ FE/src/tsx/loginpage/page.tsx
@@ -56,7 +56,7 @@ export default function LoginPage() {
 
   return (
     <div className={style.container}>
-      <header className={style.header}>배너 이미지</header>
+      <header className={style.header}>이 웹페이지는 고려대학교 동아리인 커리손으로먹기연구회와 KUCC의 협력으로 제작되었습니다.</header>
       <main className={style.main}>
         <div className={style.logoGroup}>
           <img className={style.logo} src="/curson_logo.png" alt="logo" />
