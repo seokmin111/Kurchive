6359e383ca24832e7d3ff398f99eb11286bf3d60
jwhong1020 <jwhong1020@gmail.com>
2026-03-23 21:04:01 +0900

    styles 추가

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import "./styles/global.css";
 
 // App.tsx는 src 바로 아래에 있으므로 이 경로가 맞음
 import App from "./App";
